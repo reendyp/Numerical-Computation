72f5a3861ae43245c1b61d59aab7b8b087a5de42
Rendy Putra Pratama <110386715+reendyp@users.noreply.github.com>
2024-03-27 12:02:10 +0700

    Update KN03.py

--- KN03.py
+++ KN03.py
@@ -1,3 +1,4 @@
+# Jacobi Methods
 import numpy as np
 # buat matriks persamaan linear [x,y]
 A = np.array([[2.0,3.0],
@@ -19,7 +20,7 @@ while e > 1e-6:
     for i in range(n):
         sigma = 0 
         for j in range(0,n):
-            if j != 1:
+            if j != i:
                 sigma += A[i][j]*x[j]
         xn = (Y[i]-sigma)/A[i][j]
         solAB.append(xn)
@@ -28,3 +29,34 @@ while e > 1e-6:
 
     i += 1
 print(f"akar persamaan {x} \t iterasi - {i}")
+
+# Gauss-Seidel Methods
+import numpy as np
+# buat matriks persamaan linear [x,y]
+A = np.array([[2.0,3.0],
+             [1.0,-6.0]])
+# matriks hasil persamaan linear
+y = np.array([5.0,9.0])
+
+# wadah kosong
+x = np.array([0.0,0.0])
+
+# inisiasi awal
+e = 1
+i = 0
+
+while e>1e-6:
+    solutionAx = [] # wadah solusi x1 dan x2
+    n = len(x)
+    for i in range(n):
+        sigma = 0
+        for j in range(0,i):
+            sigma += A[i][j]*solutionAx[j]
+        for j in range(i+1,n):
+            sigma += A[i][j]*x[j]
+        xn = (y[i]-sigma)/A[i][i]
+        solutionAx.append(xn)
+    e = abs(max(x)-max(solutionAx))    
+    x = np.array(solutionAx)
+    i+=1
+print(f"Hasil akar-akar = {x} iterasi - {i}")
